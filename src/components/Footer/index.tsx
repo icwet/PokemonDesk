949c9ab69f60eeb963ceed2314bf544d68fde4a7
icwet <icwet@yandex.ru>
2020-11-03 19:55:28 +0300

    add variable for footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,36 @@
 import React, { FC } from 'react';
 import styles from './index.module.scss';
 
+interface FooterItems {
+  text: string;
+  icon: string;
+}
+
 const Footer: FC = () => {
+  const footerItems: Array<FooterItems> = [
+    {
+      text: 'Make with',
+      icon: '❤️',
+    },
+    {
+      text: 'Ours Team',
+      icon: '',
+    },
+  ];
+
   return (
     <footer className={styles.Footer}>
       <div className={styles.Footer__container}>
-        <a className={styles.Footer__link} href="#">
-          Make with{' '}
-          <span aria-label="love emoji" role="img">
-            ❤️
-          </span>
-        </a>
-        <a className={styles.Footer__link} href="#">
-          Ours Team
-        </a>
+        {footerItems.map((footerItem) => (
+          <a className={styles.Footer__link} href="#">
+            {footerItem.text}{' '}
+            {footerItem.icon && (
+              <span aria-label="love emoji" role="img">
+                {footerItem.icon}
+              </span>
+            )}
+          </a>
+        ))}
       </div>
     </footer>
   );
