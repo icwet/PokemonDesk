08dee3b1c93a65756558cd2ad25b6ea7b8e665b0
icwet <icwet@yandex.ru>
2020-11-03 19:45:48 +0300

    add variable for menu data

--- src/components/Header/-Nav/index.tsx
+++ src/components/Header/-Nav/index.tsx
@@ -2,29 +2,18 @@ import React, { FC } from 'react';
 import styles from 'components/Header/index.module.scss';
 
 const HeaderNav: FC = () => {
+  const menuItems: Array<string> = ['Home', 'Pokédex', 'Legendaries', 'Documentation'];
+
   return (
     <nav className={styles.Header__nav}>
       <ul className={styles.Header__ul}>
-        <li>
-          <a className={styles.Header__link} href="#">
-            Home
-          </a>
-        </li>
-        <li>
-          <a className={styles.Header__link} href="#">
-            Pokédex
-          </a>
-        </li>
-        <li>
-          <a className={styles.Header__link} href="#">
-            Legendaries
-          </a>
-        </li>
-        <li>
-          <a className={styles.Header__link} href="#">
-            Documentation
-          </a>
-        </li>
+        {menuItems.map((menuItem) => (
+          <li>
+            <a className={styles.Header__link} href="#">
+              {menuItem}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
