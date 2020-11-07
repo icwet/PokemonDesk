7913352dff035f18464440152ce078c380d8aedc
icwet <icwet@yandex.ru>
2020-11-07 16:42:06 +0300

    refactor Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,38 @@ import Layout from 'components/Layout';
 import HeaderLogo from 'components/Header/-Logo';
 import HeaderMenu from 'components/Header/-Menu';
 import HeaderContent from 'components/Header/-Content';
+import AppLink from 'components/AppLink';
 // Styles
 import s from './index.m.scss';
+
+interface MenuItems {
+  id: number;
+  text: string;
+  href: string;
+}
+
+const menuItems: Array<Readonly<MenuItems>> = [
+  {
+    id: 0,
+    text: 'Home',
+    href: '/',
+  },
+  {
+    id: 1,
+    text: 'Pokédex',
+    href: '/pokedex',
+  },
+  {
+    id: 2,
+    text: 'Legendaries',
+    href: '#',
+  },
+  {
+    id: 3,
+    text: 'Documentation',
+    href: '#',
+  },
+];
 
 const Header: FC = () => {
   return (
@@ -13,7 +43,17 @@ const Header: FC = () => {
       <Layout>
         <HeaderContent>
           <HeaderLogo />
-          <HeaderMenu />
+          <HeaderMenu>
+            {menuItems.map((menuItem) => (
+              <li key={menuItem.id}>
+                <AppLink
+                  href={menuItem.href}
+                  linkText={menuItem.text}
+                  classNames={[s.link, { [s.active]: window.location.pathname === menuItem.href }]}
+                />
+              </li>
+            ))}
+          </HeaderMenu>
         </HeaderContent>
       </Layout>
     </header>
