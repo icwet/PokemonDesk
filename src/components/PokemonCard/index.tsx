8c34b232accbc13bbb45aa8840c702585d88942e
icwet <icwet@yandex.ru>
2020-11-10 18:23:14 +0300

    fix PokemonCard component

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,8 +1,35 @@
 import React, { FC } from 'react';
+// Components
+import { IPokemon } from 'components/PokemonCards';
+import Heading from 'components/Heading';
+import Label from 'components/Label';
+import Stat from 'components/Stat';
+import PokemonCardStats from './-Stats';
+import PokemonCardLabels from './-Labels';
+import PokemonCardPicture from './-Picture';
+// Styles
 import s from './index.m.scss';
 
-const PokemonCard: FC = ({ children }) => {
-  return <div className={s.root}>{children}</div>;
+interface PokemonCardProps {
+  pokemon: IPokemon;
+}
+
+const PokemonCard: FC<PokemonCardProps> = ({ pokemon }) => {
+  return (
+    <div className={s.root}>
+      <Heading variant="h3">{pokemon.name}</Heading>
+      <PokemonCardStats>
+        <Stat name="Attack" value={pokemon.stats.attack} />
+        <Stat name="Defence" value={pokemon.stats.defense} />
+      </PokemonCardStats>
+      <PokemonCardLabels>
+        {pokemon.abilities.map((ability, i) => (
+          <Label theme={i === 0 ? 'green' : 'blue'} value={ability} />
+        ))}
+      </PokemonCardLabels>
+      <PokemonCardPicture background={pokemon.types[0]} href={pokemon.img} />
+    </div>
+  );
 };
 
 export default PokemonCard;
--- src/components/PokemonCards/index.tsx
+++ src/components/PokemonCards/index.tsx
@@ -1,19 +1,14 @@
 import React, { FC } from 'react';
 // Components
 import PokemonCard from 'components/PokemonCard';
-import Heading from 'components/Heading';
 import Layout from 'components/Layout';
-import Label from 'components/Label';
-import Stat from 'components/Stat';
-import PokemonCardPicture, { PokemonTypes } from 'components/PokemonCard/-Picture';
-import PokemonCardLabels from 'components/PokemonCard/-Labels';
-import PokemonCardStats from 'components/PokemonCard/-Stats';
+import { PokemonTypes } from 'components/PokemonCard/-Picture';
 // Data
 import data from './data.json';
 // Styles
 import s from './index.m.scss';
 
-interface IPokemon {
+export interface IPokemon {
   nameClean: string;
   abilities: Array<string>;
   stats: {
@@ -41,19 +36,7 @@ const PokemonCards: FC = () => {
     <Layout>
       <div className={s.root}>
         {pokemons.map((pokemon: IPokemon) => (
-          <PokemonCard key={pokemon.id}>
-            <Heading variant="h3">{pokemon.name}</Heading>
-            <PokemonCardStats>
-              <Stat name="Attack" value={pokemon.stats.attack} />
-              <Stat name="Defence" value={pokemon.stats.defense} />
-            </PokemonCardStats>
-            <PokemonCardLabels>
-              {pokemon.abilities.map((ability, i) => (
-                <Label theme={i === 0 ? 'green' : 'blue'} value={ability} />
-              ))}
-            </PokemonCardLabels>
-            <PokemonCardPicture background={pokemon.types[0]} href={pokemon.img} />
-          </PokemonCard>
+          <PokemonCard pokemon={pokemon} key={pokemon.id} />
         ))}
       </div>
     </Layout>
